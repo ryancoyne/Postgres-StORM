f833972b057bf299038e86a17e3656634854731e
Ryan <ryan@buckettechnologies.com>
2018-12-23 13:41:26 -0500

    Updating the package file

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@
 import PackageDescription
 let package = Package(
 	name: "PostgresStORM",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PostgresStORM",
+            targets: ["PostgresStORM"]),
+    ],
 	dependencies: [
         .package(url: "https://github.com/ryancoyne/Perfect-PostgreSQL.git", from: "3.0.0"),
         .package(url: "https://github.com/ryancoyne/StORM-ryan.git", from: "3.0.0"),
@@ -11,8 +17,8 @@ let package = Package(
     targets: [
         .target(
             name: "PostgresStORM",
-            dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"]
-//             path: "Sources"
+            dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"],
+             path: "Sources"
         )
     ]
 )

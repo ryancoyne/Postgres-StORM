253f106bf013284492c1ef10e6e05b2c9f98be23
Ryan <ryan@buckettechnologies.com>
2018-12-22 22:33:02 -0500

    Updating the SPM package file

--- Package.swift
+++ Package.swift
@@ -4,14 +4,15 @@ import PackageDescription
 let package = Package(
 	name: "PostgresStORM",
 	dependencies: [
-        .package(url: "https://github.com/ryancoyne/Perfect-PostgreSQL", from: "3.0.0"),
-        .package(url: "https://github.com/ryancoyne/StORM-ryan", from: "3.0.0"),
-		package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+        .package(url: "https://github.com/ryancoyne/Perfect-PostgreSQL.git", from: "3.0.0"),
+        .package(url: "https://github.com/ryancoyne/StORM-ryan.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
 	],
     targets: [
         .target(
             name: "PostgresStORM",
+            path: "Sources"
             dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"]
         )
-    ],
+    ]
 )

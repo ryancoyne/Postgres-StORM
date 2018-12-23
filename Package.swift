2462e81306c538a8f60a63b5be94679e8f9f6396
Ryan <ryan@buckettechnologies.com>
2018-12-22 22:33:28 -0500

    Updating the SPM package file

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .target(
             name: "PostgresStORM",
-            path: "Sources"
+            path: "Sources",
             dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"]
         )
     ]

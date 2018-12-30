7d21eb4d4648cab86965330383d84b64fa6bd72b
Ryan <ryan@buckettechnologies.com>
2018-12-30 13:52:09 -0500

    Updating the package.swift

--- Package.swift
+++ Package.swift
@@ -11,14 +11,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ryancoyne/Perfect-PostgreSQL.git", from: "3.0.0"),
-        .package(url: "https://github.com/ryancoyne/StORM-ryan.git", from: "3.0.0"),
-	.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+        .package(url: "https://github.com/ryancoyne/StORM-ryan.git", from: "4.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
     ],
     targets: [
         .target(
             name: "PostgresStORM",
-            dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"],
-             path: "Sources"
+            dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"]
         )
     ]
 )

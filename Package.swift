fa206328edebac37321dc57bf2d34fae82006f37
Ryan <ryan@buckettechnologies.com>
2018-12-30 14:23:49 -0500

    Working on updating the package files for postgresstorm

--- Package.swift
+++ Package.swift
@@ -8,18 +8,25 @@ let package = Package(
         .library(
             name: "PostgresStORM",
             targets: ["PostgresStORM"]
-	),
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryancoyne/Perfect-PostgreSQL.git", from: "4.0.0"),
-        .package(url: "https://github.com/ryancoyne/StORM-ryan.git", from: "4.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+        .package(
+            url: "https://github.com/ryancoyne/Perfect-PostgreSQL.git",
+            from: "4.0.0"),
+        .package(
+            url: "https://github.com/ryancoyne/StORM-ryan.git",
+            from: "4.0.0"),
+        .package(
+            url: "https://github.com/PerfectlySoft/Perfect-Logger.git",
+            from: "3.0.0"
+        ),
     ],
     targets: [
         .target(
             name: "PostgresStORM",
-            dependencies: ["PerfectPostgeSQL", "StORM", "PerfectLogger"],
-	    path: "Sources"
+            dependencies: ["PerfectPostgreSQL", "StORM", "PerfectLogger"],
+            path: "Sources"
         ),
     ]
 )
